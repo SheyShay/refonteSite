4c823ca1ef3fbbb8872ecc563bf296979035cec2
Sayki <45237935+Saykix@users.noreply.github.com>
2022-01-20 14:13:48 +0100

    Add files via upload

---
+++ siteObservatoire/galerie/galerie.js
@@ -0,0 +1,13 @@
+let hamburgerMenuState='close';
+
+function doClickHamburger(){
+    console.log('On est là!');
+    console.log(hamburgerMenuState);
+    if(hamburgerMenuState=='close'){
+        document.getElementById("menuHamburger").style.display = "flex";
+        hamburgerMenuState='open';
+    }else{
+        document.getElementById("menuHamburger").style.display = "none";
+        hamburgerMenuState='close';
+    }
+}
